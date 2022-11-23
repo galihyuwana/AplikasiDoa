1a9657b7a465fa4acf370a15e5f30e5366010b4f
galihyuwana <galihe430@gmail.com>
2022-11-23 14:27:12 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Home from './screen/Home'
+
+const App = () => {
+  return (
+    <View>
+      <Home/>
+    </View>
+  )
+}
+
+export default App
